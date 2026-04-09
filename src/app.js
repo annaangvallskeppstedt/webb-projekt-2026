0683c886cbf4a03c7ad0ef9d90896b119eff9d35
Anna Angvall-Skeppstedt <anna.angvallskeppstedt@gmail.com>
2026-04-09 15:52:21 +0200

    add back booking

--- src/app.js
+++ src/app.js
@@ -31,6 +31,6 @@ app.get("/health", (req, res) => {
 app.use("/products", productsRouter);
 app.use("/auth", authRouter);
 app.use("/events", eventsRouter);
-// app.use("/bookings", bookingsRouter);
+app.use("/bookings", bookingsRouter);
 
 export default app;
